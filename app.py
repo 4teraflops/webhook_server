fdaaee72d1ea4f07bed3875b0dcec2722448844e
4teraflops <3lL05WzAr2UA>
2020-11-24 13:32:43 +0500

    добавлен мониторинг фискализации

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import config
 from loguru import logger
 import requests
 import json
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
-logger.add(f'src/{__name__}.log', format='{time} {level} {message}', level='DEBUG', rotation='10 MB', compression='zip')
+logger.add(f'log/{__name__}.log', format='{time} {level} {message}', level='DEBUG', rotation='10 MB', compression='zip')
 
 
 @logger.catch  # Логировать исключения из функции
@@ -16,7 +17,10 @@ logger.add(f'src/{__name__}.log', format='{time} {level} {message}', level='DEBU
 def finmonstate():
     if request.method == 'POST':
         data = request.get_json()
-        key = data['key']
+        try:
+            key = data['key']
+        except AttributeError:
+            return 'Incorrect key'
 
         if key == config.key:
             state = data['state']
@@ -25,17 +29,46 @@ def finmonstate():
             except KeyError:
                 count = ''
             now = datetime.now()
-            time = now.strftime('%d-%m-%Y %H:%M:%S')
+            time = now.strftime('%Y-%m-%d %H:%M:%S')
             #logger.debug(f'data: {data}\nkey: {key}\nstate: {state}\ntime: {time}')
-            record_data(time, state, count)
+            table = 'finmon_states'
+            record_data(table, state, time, count)
             return 'Data Recorded'
+        else:
+            return 'Incorrect key'
+
+
+@logger.catch  # Логировать исключения из функции
+@app.route('/fiscalization', methods=['POST'])
+def fiscalization():
+    if request.method == 'POST':
+        data = request.get_json()
+        try:
+            key = data['key']
+        except AttributeError:
+            return 'Incorrect key'
+
+        if key == config.fisc_key:
+            state = data['state']
+            try:
+                count = data['count']
+            except KeyError:
+                count = ''
+            now = datetime.now()
+            time = now.strftime('%Y-%m-%d %H:%M:%S')
+            #logger.debug(f'data: {data}\nkey: {key}\nstate: {state}\ntime: {time}')
+            table = 'fiscalization_states'
+            record_data(table, state, time, count)
+            return 'Data Recorded'
+        else:
+            return 'Incorrect key'
 
 
 @logger.catch
-def record_data(time, state, count):
+def record_data(table, state, time, count):
     conn = sqlite3.connect('src/db.sqlite')
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO finmon_states VALUES (Null, '{state}', '{time}', '{count}')")
+    cursor.execute(f"INSERT INTO {table} VALUES (Null, '{state}', '{time}', '{count}')")
     conn.commit()
 
 
@@ -47,7 +80,11 @@ def do_alarm(t_alarmtext):
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=5000, debug=False)
+        # Debug/Development
+        # app.run(debug=True, host="0.0.0.0", port="5000")
+        # Production
+        http_server = WSGIServer(('', 5000), app)
+        http_server.serve_forever()
     except Exception as e:
         t_alarmtext = f'Webhook server (app.py): {str(e)}'
         do_alarm(t_alarmtext)
